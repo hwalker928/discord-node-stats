9ed550d86aba12d59be2c0a8a9d781453b00f43d
hwalker928 <harry@repoleved.xyz>
2021-01-15 21:13:21 +0000

    Tidied code + added toggle

--- bot/main.py
+++ bot/main.py
@@ -10,6 +10,9 @@ intents.members = True
 bot = commands.Bot(command_prefix="!", intents=intents)
 with open('config.json') as bot.config_jsonfile:
     bot.config_json = json.load(bot.config_jsonfile)
+    bot.update_channel = bot.get_channel(bot.config_json["channelUpdates"])
+    bot.node_owner = bot.get_user(bot.config_json["nodeOwnerID"])
+    bot.node_name = bot.config_json["nodeName"]
 
 @tasks.loop(seconds=60)
 async def server_checker():
@@ -35,14 +38,33 @@ async def server_idle(cpu, ram):
 
 async def server_down():
     await bot.change_presence(status=discord.Status.dnd, activity=discord.Game(name=f'OFFLINE'))
-    chnl = bot.get_channel(bot.config_json["channelUpdates"])
-    usr = bot.get_user(bot.config_json["nodeOwnerID"])
-    nodeName = bot.config_json["nodeName"]
-    await chnl.send(f":warning: {usr.mention}, {nodeName} is down! :warning:")
+    await bot.update_channel.send(f":warning: {bot.node_owner.mention}, {bot.node_name} is down! :warning:")
 
 @bot.event
 async def on_ready():
-    print("started")
+    print("The monitoring has started.")
     server_checker.start()
+    bot.task_active = True
+
+async def nodeOwnerCheck(ctx):
+    return ctx.message.author.id == bot.node_owner.id
+
+@bot.command(aliases=["m", "disable", "noalert", "noalerts", "turnoff", "shutdown", "start", "turnon", "alerts", "alertsenable", "enable"])
+@commands.check(nodeOwnerCheck)
+async def maintanance(ctx):
+    if bot.task_active:
+        server_checker.stop()
+        bot.task_active = False
+        await ctx.send(":x: Monitoring disabled.")
+    else:
+        server_checker.start()
+        bot.task_active = True
+        await ctx.send(":white_check_mark: Monitoring enabled.")
+
+@bot.event
+async def on_command_error(ctx, error):
+    await ctx.send("You must be the node owner to run this!")
+
+
 
 bot.run(bot.config_json["token"])
